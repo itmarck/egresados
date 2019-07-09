f95c9f243cc878dd403a4226cd8decc3581a74bf
Marcelo Velasquez <marceloo.velasquez@hotmail.com>
2019-07-09 12:02:18 -0500

    Egresado, esquema basico

---
+++ cliente/src/views/Egresado/Postgrado.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-container grid-list-lg>
+    <v-layout row wrap>
+      <v-flex>
+        <v-card> <!-- Postgrado -->
+          <v-card-text>
+            Postgrado
+          </v-card-text>
+        </v-card>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
