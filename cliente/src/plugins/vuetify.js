fb81c14d24e1d8484036130f3181a427022b5b77
Marcelo Velasquez <marceloo.velasquez@hotmail.com>
2019-07-09 19:02:21 -0500

    Tema de vuetify colores agregados

--- cliente/src/plugins/vuetify.js
+++ cliente/src/plugins/vuetify.js
@@ -4,4 +4,11 @@ import 'vuetify/src/stylus/app.styl'
 
 Vue.use(Vuetify, {
   iconfont: 'md',
+  theme: {
+    primary: '#0D47A1',
+    secondary: '#181818',
+    accent: '#181818',
+    // accent: '#F4BE1D',
+    error: '#aa1515'
+  }
 })
