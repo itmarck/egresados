2497a75f0856c8603354f76f5440d61156404637
Marcelo Velasquez <marceloo.velasquez@hotmail.com>
2019-07-17 16:46:43 -0500

    Toolbar publico

---
+++ cliente/src/components/PublicToolbar.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-toolbar color="primary" dark app>
+    <v-toolbar-title>
+      <span class="font-weight-black">UNPRG </span>
+      <span class="font-weight-light">EGRESADOS</span>
+    </v-toolbar-title>
+    <v-spacer></v-spacer>
+    <v-toolbar-items>
+      <v-btn v-if="this.$route.path == '/registro'" flat to="login">
+        Ingresar
+      </v-btn>
+      <v-btn v-else flat to="registro">Registrarse</v-btn>
+    </v-toolbar-items>
+  </v-toolbar>
+</template>
