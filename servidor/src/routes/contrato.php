d34f95544370354ac57d7e2b0e447b31e02d25d2
JavCho23 <javier120699lili@gmail.com>
2019-07-16 19:38:46 -0500

    contratos por dni

--- servidor/src/routes/contrato.php
+++ servidor/src/routes/contrato.php
@@ -22,25 +22,46 @@ $app->get('/api/contratos', function () {
   }
 });
 
-$app->get('/api/contratos/{codigo}',function(Request $request){
-   $codigo = $request->getAttribute('codigo');
-  try {
-    $data = $this->db->query("SELECT CONCAT(P.nombres,' ',P.apellidoPaterno,' ',P.apellidoPaterno) as Nombre,C.codigo,codigoEgresado,EP.nombre as Carrera,codigoCentroLaboral,Cen.razonSocial as Centrolaboral,cargo,C.fechaInicio,C.fechaTermino,detalleFunciones 
-                              FROM contrato C
-                              INNER JOIN egresado E on E.codigo = C.codigoEgresado
-                              INNER JOIN persona P on P.codigo = E.codigoPersona
-                              INNER JOIN escuelaprofesional EP on EP.codigo = E.codigoEscuela
-                              INNER JOIN centrolaboral Cen on Cen.codigo  = C.codigoCentroLaboral
-                              WHERE C.codigo = $codigo and C.vigencia=1")->fetchAll();
-    if ($data) {
-      $result = array('estado' => true, 'data' => $data);
-      echo json_encode($result);
-   }else {
-     echo json_encode( array('estado' => false ));
-   }
-  } catch (PDOException $e) {
-    echo '{"Error": { "mensaje": '. $e->getMessage().'}';
+// $app->get('/api/contratos/{codigo}',function(Request $request){
+//    $codigo = $request->getAttribute('codigo');
+//   try {
+//     $data = $this->db->query("SELECT CONCAT(P.nombres,' ',P.apellidoPaterno,' ',P.apellidoPaterno) as Nombre,C.codigo,codigoEgresado,EP.nombre as Carrera,codigoCentroLaboral,Cen.razonSocial as Centrolaboral,cargo,C.fechaInicio,C.fechaTermino,detalleFunciones 
+//                               FROM contrato C
+//                               INNER JOIN egresado E on E.codigo = C.codigoEgresado
+//                               INNER JOIN persona P on P.codigo = E.codigoPersona
+//                               INNER JOIN escuelaprofesional EP on EP.codigo = E.codigoEscuela
+//                               INNER JOIN centrolaboral Cen on Cen.codigo  = C.codigoCentroLaboral
+//                               WHERE C.codigo = $codigo and C.vigencia=1")->fetchAll();
+//     if ($data) {
+//       $result = array('estado' => true, 'data' => $data);
+//       echo json_encode($result);
+//    }else {
+//      echo json_encode( array('estado' => false ));
+//    }
+//   } catch (PDOException $e) {
+//     echo '{"Error": { "mensaje": '. $e->getMessage().'}';
+//   }
+// });
+
+$app->get('/api/contratos/{dniPersona}',function(Request $request){
+  $codigo = $request->getAttribute('dniPersona');
+ try {
+   $data = $this->db->query("SELECT CONCAT(P.nombres,' ',P.apellidoPaterno,' ',P.apellidoPaterno) as Nombre,C.codigo,codigoEgresado,EP.nombre as Carrera,codigoCentroLaboral,Cen.razonSocial as Centrolaboral,cargo,C.fechaInicio,C.fechaTermino,detalleFunciones 
+                             FROM contrato C
+                             INNER JOIN egresado E on E.codigo = C.codigoEgresado
+                             INNER JOIN persona P on P.codigo = E.codigoPersona
+                             INNER JOIN escuelaprofesional EP on EP.codigo = E.codigoEscuela
+                             INNER JOIN centrolaboral Cen on Cen.codigo  = C.codigoCentroLaboral
+                             WHERE P.dni = $codigo and C.vigencia=1")->fetchAll();
+   if ($data) {
+     $result = array('estado' => true, 'data' => $data);
+     echo json_encode($result);
+  }else {
+    echo json_encode( array('estado' => false ));
   }
+ } catch (PDOException $e) {
+   echo '{"Error": { "mensaje": '. $e->getMessage().'}';
+ }
 });
 
 $app->post('/api/contratos/add',function(Request $request){
