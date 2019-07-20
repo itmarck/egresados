b952395a00ea7965767d05ca75af783c9cb2cf8f
Marcelo Velasquez <marceloo.velasquez@hotmail.com>
2019-07-20 18:10:54 -0500

    Listo para solo agregar vistas

--- cliente/src/views/Personal/Mantenimiento.vue
+++ cliente/src/views/Personal/Mantenimiento.vue
@@ -1,13 +1,19 @@
 <template>
   <v-container grid-list-lg>
-    <v-layout row wrap>
-      <v-flex xs12>
-        <v-card>
-          <v-card-text>
-            Aca van todos los mantenimientos
-          </v-card-text>
-        </v-card>
-      </v-flex>
-    </v-layout>
+    <mant-universidades v-if="tabla == 'Universidades'" />
+    <mant-escuelas v-else-if="tabla == 'Escuelas'" />
   </v-container>
 </template>
+
+<script>
+import { mapState } from "vuex";
+export default {
+  components: {
+    MantUniversidades: () => import("../../components/MantUniversidades"),
+    MantEscuelas: () => import("../../components/MantEscuelas")
+  },
+  computed: {
+    ...mapState("mantenimiento", ["tabla"])
+  }
+};
+</script>
