3311d75ca8fcc77f6eb82209209545b124f9cef7
Marcelo Velasquez <marceloo.velasquez@hotmail.com>
2019-07-21 01:38:38 -0500

    validar usuario al iniciar la aplicacion

--- cliente/src/App.vue
+++ cliente/src/App.vue
@@ -3,3 +3,21 @@
     <router-view />
   </v-app>
 </template>
+
+<script>
+import { get, getUser, removeUser } from "./bd/api";
+export default {
+  created() {
+    if (!getUser()) return;
+    get("personas/" + getUser().dni).then(res => {
+      if (res.estado == true) {
+        if (parseInt(res.data.vigencia) == 0) {
+          removeUser();
+          this.$router.push("/login");
+        }
+      }
+    });
+  }
+};
+</script>
+
