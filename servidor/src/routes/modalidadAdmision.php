b6cc059202381f2aef6e2bfe73d7085141199120
JavCho23 <javier120699lili@gmail.com>
2019-07-23 04:05:23 -0500

    actualizar

--- servidor/src/routes/modalidadAdmision.php
+++ servidor/src/routes/modalidadAdmision.php
@@ -17,3 +17,23 @@ $app->put('/api/modalidadesAdmision/{codigo}', function (Request $request) {
   $nombre = $request->getParam('nombre');
   $nombre = $request->getParam('descripcion');
 });
+
+$app->put('/api/modadlidadAdmision/{codigo}', function (Request $request) {
+  $codigo = $request->getAttribute('codigo');
+  $nombre = $request->getParam('nombre');
+  $descripcion = $request->getParam('descripcion');
+  try {
+    $cantidad = $this->db->exec("UPDATE modalidadAdmision set
+                                nombre ='$nombre',
+                                descripcion= '$descripcion',
+                                vigencia= 1  
+                                WHERE codigo = $codigo");
+    if ($cantidad > 0) {
+      echo json_encode(array('estado' => true, 'mensaje' => 'Modalidad actualizado'));
+    } else {
+      echo json_encode(array('estado' => false, 'mensaje' => 'No se pudo actualizar '));
+    }
+  } catch (PDOException $e) {
+    echo json_encode(array('estado' => false, 'mensaje' => 'Error al conectar con la base de datos'));
+  }
+});
