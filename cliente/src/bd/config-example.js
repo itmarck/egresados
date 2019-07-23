b6f28671e9550590ad7c8bf3691a55c4e433dd76
Marcelo Velasquez <marceloo.velasquez@hotmail.com>
2019-07-23 06:44:27 -0500

    Archivo de config de la bd modificado

---
+++ cliente/src/bd/config-example.js
@@ -0,0 +1,6 @@
+// URL de la api -- Cambiar de ser necesario
+
+const host = 'http://192.168.1.8/';
+
+export const url = host + 'egresados/servidor/src/public/api/';
+export const urlImage = host + 'egresados/servidor/src/public/';
