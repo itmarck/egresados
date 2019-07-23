852c22c40fa390f697f6b4a5cdc52a69b2694ebc
JavCho23 <javier120699lili@gmail.com>
2019-07-23 03:49:42 -0500

    Editar

--- servidor/src/routes/departamento.php
+++ servidor/src/routes/departamento.php
@@ -9,3 +9,21 @@ $app->get('/api/departamentos', function () {
     echo json_encode(array('estado' => false));
   }
 });
+
+$app->put('/api/departamentos/{codigo}', function (Request $request) {
+  $codigo = $request->getAttribute('codigo');
+  $nombre = $request->getParam('nombre');
+  try {
+    $cantidad = $this->db->exec("UPDATE departamento set
+                                nombre ='$nombre',
+                                vigencia= 1  
+                                WHERE codigo = $codigo");
+    if ($cantidad > 0) {
+      echo json_encode(array('estado' => true, 'mensaje' => 'Departamento actualizado'));
+    } else {
+      echo json_encode(array('estado' => false, 'mensaje' => 'No se pudo actualizar '));
+    }
+  } catch (PDOException $e) {
+    echo json_encode(array('estado' => false, 'mensaje' => 'Error al conectar con la base de datos'));
+  }
+});
