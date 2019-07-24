550157a5217d3f7a54188068526b1475a3f8b75b
Marcelo Velasquez <marceloo.velasquez@hotmail.com>
2019-07-24 18:26:42 -0500

    Config: Cambio de la ruta urlImage

--- cliente/src/bd/config-example.js
+++ cliente/src/bd/config-example.js
@@ -3,4 +3,4 @@
 const host = 'http://192.168.1.8/';
 
 export const url = host + 'egresados/servidor/src/public/api/';
-export const urlImage = host + 'egresados/servidor/src/public/';
+export const urlImage = host + 'egresados/servidor/src/public/images/';
