3c95216dd3f0895e834cbc83328211daa66a8fe3
Marcelo Velasquez <marceloo.velasquez@hotmail.com>
2019-07-25 23:11:57 -0500

    Perfil vista de personal

--- cliente/src/views/Personal/Perfil.vue
+++ cliente/src/views/Personal/Perfil.vue
@@ -1,14 +1,268 @@
 <template>
   <v-container grid-list-lg>
-    <v-layout row wrap>
-      <v-flex>
-        <v-card>
-          <!-- Datos de egresado -->
-          <v-card-text>
-            Perfil del personal
-          </v-card-text>
-        </v-card>
+    <v-layout row wrap justify-center v-if="persona != null">
+      <!-- Vista del perfil -->
+      <v-flex xs12 xl7>
+        <v-layout row wrap justify-center>
+          <v-flex>
+            <v-card>
+              <v-img :src="urlFoto" aspect-ratio="1" />
+            </v-card>
+          </v-flex>
+          <v-flex xs12 sm8 lg9 xl8>
+            <v-card>
+              <v-container>
+                <v-card-title class="title font-weight-light">
+                  <span>
+                    {{ persona.nombres }} <br />
+                    {{ persona.apellidoPaterno }}
+                    {{ persona.apellidoMaterno }}
+                  </span>
+                  <v-spacer></v-spacer>
+                  <v-btn flat icon @click="editar">
+                    <v-icon>edit</v-icon>
+                  </v-btn>
+                </v-card-title>
+                <v-card-text>
+                  <v-layout column align-start pl-2 pb-3>
+                    <p class="body-1">
+                      <span class="font-weight-medium"> DNI </span>
+                      <span class="font-weight-light"> {{ persona.dni }}</span>
+                    </p>
+                    <div class="body-1">
+                      <v-icon small left>email</v-icon>
+                      <span class="font-weight-light">
+                        {{ persona.correo }}
+                      </span>
+                    </div>
+                    <div class="body-1">
+                      <v-icon small left>phone_android</v-icon>
+                      <span class="font-weight-light">
+                        {{ persona.celular }}
+                      </span>
+                    </div>
+                  </v-layout>
+                </v-card-text>
+              </v-container>
+            </v-card>
+          </v-flex>
+        </v-layout>
+      </v-flex>
+      <v-flex xs12 xl5 v-if="isEdit">
+        <v-form @submit.prevent="guardar">
+          <v-layout row wrap>
+            <v-flex xs12>
+              <v-card>
+                <v-card-text>
+                  <v-card>
+                    <v-card-text>
+                      <v-text-field
+                        v-model="nombres"
+                        label="Ingrese nombres"
+                        placeholder="Nombres"
+                        type="text"
+                      />
+                      <v-text-field
+                        v-model="paterno"
+                        label="Ingrese apellido paterno"
+                        placeholder="Apellido paterno"
+                        type="text"
+                      />
+                      <v-text-field
+                        v-model="materno"
+                        label="Ingrese apellido materno"
+                        placeholder="Apellido materno"
+                        type="text"
+                      />
+                    </v-card-text>
+                  </v-card>
+
+                  <v-radio-group
+                    v-model="genero"
+                    row
+                    label="Genero "
+                    prepend-icon="person"
+                  >
+                    <v-radio label="Masculino" value="1"></v-radio>
+                    <v-radio label="Femenino" value="0"></v-radio>
+                  </v-radio-group>
+                  <v-text-field
+                    v-model="correo"
+                    prepend-icon="email"
+                    label="Ingrese correo"
+                    placeholder="Correo"
+                    type="email"
+                    hint="Este correo te va servir para recuperar la contraseña"
+                  />
+                  <v-text-field
+                    v-model="celular"
+                    prepend-icon="phone_android"
+                    label="Ingrese número de celular"
+                    placeholder="Número de celular"
+                    type="text"
+                  />
+                  <v-text-field
+                    label="Seleccione foto de perfil"
+                    @click="pickFile"
+                    v-model="imageName"
+                    prepend-icon="photo"
+                  ></v-text-field>
+                  <input
+                    type="file"
+                    style="display: none"
+                    ref="image"
+                    accept="image/*"
+                    @change="onFilePicked"
+                  />
+                </v-card-text>
+              </v-card>
+            </v-flex>
+            <v-flex xs12>
+              <v-btn block type="submit">Guardar cambios</v-btn>
+            </v-flex>
+          </v-layout>
+        </v-form>
       </v-flex>
     </v-layout>
   </v-container>
 </template>
+
+<script>
+import { get, put, patch, setUser, getUser, uploadPhoto } from "../../bd/api";
+import { urlImage } from "../../bd/config";
+import { mapState, mapMutations } from "vuex";
+export default {
+  data: () => ({
+    isEdit: false,
+    persona: null,
+    correo: "",
+    nombres: "",
+    paterno: "",
+    materno: "",
+    genero: "1",
+    celular: "",
+
+    imageName: "",
+    imageUrl: "",
+    imageFile: ""
+  }),
+  computed: {
+    ...mapState(["user"]),
+    urlFoto() {
+      return urlImage + this.user.urlFoto;
+    },
+    privacidad() {
+      if (this.persona) return parseInt(this.persona.privacidad);
+    }
+  },
+  methods: {
+    ...mapMutations(["snackbar"]),
+    validar() {
+      if (this.nombres == "" || this.paterno == "" || this.materno == "") {
+        this.snackbar("Ingrese los nombres completos");
+        return false;
+      }
+      if (this.correo == "") {
+        this.snackbar("Ingrese correo");
+        return false;
+      }
+      return true;
+    },
+    guardar() {
+      if (!this.validar()) return;
+      let datos = {
+        dni: this.persona.dni,
+        nombres: this.nombres,
+        paterno: this.paterno,
+        materno: this.materno,
+        genero: this.genero,
+        fecha: this.fechaNacimiento,
+        celular: this.celular,
+        correo: this.correo,
+        estado: this.estadoCivil
+      };
+      put("personal/" + this.persona.codigo, datos).then(res => {
+        let respuesta = res.mensaje;
+        if (res.estado == true) {
+          if (this.imageFile != "")
+            uploadPhoto(
+              "personal/images/" + this.user.codigo,
+              this.imageFile
+            ).then(res => {
+              this.cargarDatos();
+              this.snackbar(res.mensaje + " " + respuesta);
+            });
+          this.cargarDatos();
+          this.isEdit = false;
+        }
+        this.snackbar(respuesta);
+      });
+    },
+    actualizarUsuario() {
+      let user = getUser();
+      let newUser = {
+        ...user,
+        nombres: this.persona.nombres,
+        apellidoPaterno: this.persona.apellidoPaterno,
+        apellidoMaterno: this.persona.apellidoMaterno,
+        nombre:
+          this.persona.nombres +
+          " " +
+          this.persona.apellidoPaterno +
+          " " +
+          this.persona.apellidoMaterno,
+        urlFoto: this.persona.urlFoto
+      };
+      setUser(newUser);
+    },
+    editar() {
+      this.copiarDatos();
+      this.isEdit = true;
+    },
+    copiarDatos() {
+      this.isEdit = true;
+      this.correo = this.persona.correo;
+      this.nombres = this.persona.nombres;
+      this.paterno = this.persona.apellidoPaterno;
+      this.materno = this.persona.apellidoMaterno;
+      this.genero = this.persona.genero;
+      this.fechaNacimiento = this.persona.fechaNacimiento;
+      this.celular = this.persona.celular;
+      this.estadoCivil = this.persona.estadoCivil;
+    },
+    cargarDatos() {
+      get("personal/" + this.user.codigo).then(res => {
+        this.persona = res.data[0];
+        this.actualizarUsuario();
+      });
+    },
+    onFilePicked(e) {
+      const files = e.target.files;
+      if (files[0] !== undefined) {
+        this.imageName = files[0].name;
+        if (this.imageName.lastIndexOf(".") <= 0) {
+          return;
+        }
+        const fr = new FileReader();
+        fr.readAsDataURL(files[0]);
+        fr.addEventListener("load", () => {
+          this.imageUrl = fr.result;
+          this.imageFile = files[0];
+        });
+      } else {
+        this.imageName = "";
+        this.imageFile = "";
+        this.imageUrl = "";
+      }
+    },
+    pickFile() {
+      this.$refs.image.click();
+    }
+  },
+  created() {
+    this.cargarDatos();
+  }
+};
+</script>
+
+
