45377c95927acea5fef9f63d9579491e88420d4f
JavCho23 <javier120699lili@gmail.com>
2019-07-30 14:54:48 -0500

    arreglos pedidos

--- servidor/src/routes/reportes.php
+++ servidor/src/routes/reportes.php
@@ -29,20 +29,23 @@ $app->get('/api/reporte/admision/{admision}', function (Request $request) {
 $app->get('/api/reporte/dni/{dni}', function (Request $request) {
   $codigo = $request->getAttribute('dni');
   try {
-    $egresado = $this->db->query("SELECT codigo,CONCAT(nombres,' ',apellidoPaterno,' ',apellidoMaterno) as Nombre,celular,correo,urlFoto
+    $egresado = $this->db->query("SELECT codigo,CONCAT(nombres,' ',apellidoPaterno,' ',apellidoMaterno) as nombre,celular,correo,urlFoto
     FROM persona WHERE (DNI = $codigo or codigo = $codigo) and vigencia=1")->fetchAll();
-    $carreras = $this->db->query("SELECT E.nombre, A.nombre as admision, YEAR(Eg.fechaTermino) as FechaEgreso, C.codigo as colegiatura 
-              FROM egresado Eg INNER JOIN escuelaProfesional E ON Eg.codigoEscuela = E.codigo
-              INNER JOIN admision A ON Eg.codigoAdmision = A.codigo 
-              INNER JOIN colegiatura C ON C.codigoEgresado = Eg.codigo
-              INNER JOIN persona P on Eg.codigoPersona = P.codigo
-              WHERE (DNI = $codigo or P.codigo = $codigo) ")->fetchAll();
-    $estudiosPost = $this->db->query("SELECT Pt.nombre, Pt.fechaTermino
+    $carreras = $this->db->query("SELECT E.nombre, A.nombre as admision, CONCAT(YEAR(Eg.fechaInicio),'-',YEAR(Eg.fechaTermino)) as fecha, C.codigo as colegiatura, C.fecha as fechaColegiatura,MA.nombre  as modalidad
+                                  FROM egresado Eg INNER JOIN escuelaProfesional E ON Eg.codigoEscuela = E.codigo
+                                  INNER JOIN admision A ON Eg.codigoAdmision = A.codigo 
+                                  INNER JOIN modalidadAdmision MA on MA.codigo= A.codigoModalidad
+                                  INNER JOIN colegiatura C ON C.codigoEgresado = Eg.codigo
+                                  INNER JOIN persona P on Eg.codigoPersona = P.codigo
+                                  WHERE (DNI = $codigo or P.codigo = $codigo) ")->fetchAll();
+    $estudiosPost = $this->db->query("SELECT Pt.nombre, CONCAT(YEAR(Pt.fechaInicio),'-',YEAR(Pt.fechaTermino)) as fecha, IF(CE.razonSocial is null ,U.nombre,CE.razonSocial) as lugar
                                       FROM estudiosPostgrado Pt
                                       INNER JOIN egresado E on E.codigo =  Pt.codigoEgresado
                                       INNER JOIN persona P on E.codigoPersona = P.codigo
+                                      LEFT JOIN centroEstudios CE on  CE.codigo = Pt.codigoCentroEstudios
+                                      LEFT JOIN universidad U on U.codigo = Pt.codigoUniversidad 
                                       WHERE (DNI = $codigo or P.codigo = $codigo)")->fetchAll();
-    $laboral = $this->db->query("SELECT CONCAT(A.nombre,', ',D.nombre ) as descripcion, cargo, razonSocial as nombre
+    $laboral = $this->db->query("SELECT A.nombre as actividad , D.nombre  as distrito, cargo, razonSocial as nombre, YEAR(C.fechainicio) as fechaInicio
                                 FROM persona P
                                 INNER JOIN egresado E on E.codigoPersona = P.codigo
                                 INNER JOIN contrato C on C.codigoEgresado = E.codigo
