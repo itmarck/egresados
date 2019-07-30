c7f34756ff4bde337bb11406bcdb78095e1ea437
JavCho23 <javier120699lili@gmail.com>
2019-07-30 18:28:39 -0500

    fix getmessage

--- servidor/src/routes/persona.php
+++ servidor/src/routes/persona.php
@@ -57,14 +57,14 @@ $app->get('/api/personas-publico', function () {
                               INNER JOIN escuelaprofesional EP on EP.codigo = E.codigoEscuela
                               WHERE P.privacidad = 0
                               ORDER BY fechaTermino DESC
-                              LIMIT 25")->fetchAll();
+                              LIMIT 20")->fetchAll();
     $postgrados = $this->db->query("SELECT CONCAT(nombres,' ',apellidoPaterno,' ',apellidoMaterno) as nombres,urlFoto, EP.fechaTermino, EP.nombre, 'Estudio de postgrado' as tipo
                                     FROM persona P 
                                     INNER JOIN egresado E ON E.codigoPersona = P.codigo
                                     INNER JOIN estudiospostgrado EP on EP.codigoEgresado = E.codigo
                                     WHERE P.privacidad = 0
                                     ORDER BY fechaTermino DESC
-                                    LIMIT 25")->fetchAll();
+                                    LIMIT 20")->fetchAll();
     $data = array_merge($carreras, $postgrados);
     usort($data, 'ordenar');
     $data = array_slice($data, 0, 20);
