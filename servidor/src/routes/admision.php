d56f2de68047dcbacd7a7b37b521ecb124d15eee
JavCho23 <javier120699lili@gmail.com>
2019-07-30 21:59:18 -0500

    mantenimiento admisiones

--- servidor/src/routes/admision.php
+++ servidor/src/routes/admision.php
@@ -7,7 +7,7 @@ $app->get('/api/admisiones', function () {
   try {
     $data = $this->db->query("SELECT A.codigo,codigoEscuela,date_format(fechaAdmision,'%Y/%m/%d') as fechaAdmision,A.nombre as ciclo,codigoModalidad, M.nombre as modalidad 
                             FROM admision A
-                            INNER JOIN modalidadAdmision M on A.codigoModalidad = M.codigo  
+                            INNER JOIN modalidadAdmision M on A.codigoModalidad = M.codigo 
                             WHERE A.vigencia=1
                             ORDER BY fechaAdmision")->fetchAll();
     if ($data) {
@@ -17,15 +17,16 @@ $app->get('/api/admisiones', function () {
       echo json_encode(array('estado' => false, 'mensaje' => 'No se han encontrado admisiones', 'data' => []));
     }
   } catch (PDOException $e) {
-    echo json_encode(array('estado' => false,'mensaje'=>'Error al conectar con la base de datos', 'data' => []));
+    echo json_encode(array('estado' => false, 'mensaje' => 'Error al conectar con la base de datos', 'data' => []));
   }
 });
 
 $app->get('/api/admisiones-all', function () {
   try {
-    $data = $this->db->query("SELECT A.codigo,codigoEscuela,date_format(fechaAdmision,'%Y/%m/%d') as fechaAdmision,A.nombre as ciclo,codigoModalidad, M.nombre as modalidad,A.vigencia
+    $data = $this->db->query("SELECT A.codigo,codigoEscuela as escuela,EP.codigoUniversidad as universidad ,date_format(fechaAdmision,'%Y/%m/%d') as fecha,A.nombre ,codigoModalidad as modalidad, M.nombre as modalidad,A.vigencia
                             FROM admision A
                             INNER JOIN modalidadAdmision M on A.codigoModalidad = M.codigo
+                            INNER JOIN escuelaProfesional EP on EP.codigo = codigoEscuela 
                             ORDER BY fechaAdmision")->fetchAll();
     if ($data) {
       $result = array('estado' => true, 'data' => $data);
@@ -34,7 +35,7 @@ $app->get('/api/admisiones-all', function () {
       echo json_encode(array('estado' => false, 'mensaje' => 'No se han encontrado admisiones', 'data' => []));
     }
   } catch (PDOException $e) {
-    echo json_encode(array('estado' => false,'mensaje'=>'Error al conectar con la base de datos', 'data' => []));
+    echo json_encode(array('estado' => false, 'mensaje' => 'Error al conectar con la base de datos', 'data' => []));
   }
 });
 
@@ -56,48 +57,48 @@ $app->get('/api/admisiones/{nombreEscuela}/{nombreUniversidad}', function (Reque
       echo json_encode(array('estado' => false, 'data' => []));
     }
   } catch (PDOException $e) {
-    echo json_encode(array('estado' => false,'mensaje'=>'Error al conectar con la base de datos', 'data' => []));
+    echo json_encode(array('estado' => false, 'mensaje' => 'Error al conectar con la base de datos', 'data' => []));
   }
 });
 
 $app->post('/api/admisiones/add', function (Request $request) {
   $nombre = $request->getParam('nombre');
-  $codigoEscuela = $request->getParam('codigoEscuela');
-  $fechaAdmision = $request->getParam('fechaAdmision');
-  $codigoModalidad = $request->getParam('codigoModalidad');
+  $codigoEscuela = $request->getParam('escuela');
+  $fechaAdmision = $request->getParam('admision');
+  $codigoModalidad = $request->getParam('modalidad');
   try {
     $cantidad = $this->db->exec("INSERT INTO admision(codigoEscuela,fechaAdmision,nombre,codigoModalidad,vigencia) 
                             Values($codigoEscuela,'$fechaAdmision','$nombre',$codigoModalidad,1)");
     if ($cantidad > 0) {
-      echo json_encode(array('estado' => true));
+      echo json_encode(array('estado' => true, 'mensaje' => 'Admision agregada'));
     } else {
-      echo json_encode(array('estado' => false));
+      echo json_encode(array('estado' => false, 'mensaje' => 'Error al agregar admision'));
     }
   } catch (PDOException $e) {
-    echo json_encode(array('estado' => false,'mensaje'=>'Error al conectar con la base de datos'));
+    echo json_encode(array('estado' => false, 'mensaje' => 'Error al conectar con la base de datos'));
   }
 });
 
 $app->put('/api/admisiones/{codigo}', function (Request $request) {
   $codigo = $request->getAttribute('codigo');
   $nombre = $request->getParam('nombre');
-  $codigoEscuela = $request->getParam('codigoEscuela');
-  $fechaAdmision = $request->getParam('fechaAdmision');
-  $codigoModalidad = $request->getParam('codigoModalidad');
+  $codigoEscuela = $request->getParam('escuela');
+  $fechaAdmision = $request->getParam('admision');
+  $codigoModalidad = $request->getParam('modalidad');
   try {
     $cantidad = $this->db->exec("UPDATE admision set
                                 nombre ='$nombre',
-                                codigoEscuela = '$codigoEscuela',
+                                codigoEscuela = $codigoEscuela,
                                 fechaAdmision = '$fechaAdmision',
-                                codigoModalidad = '$codigoModalidad'
+                                codigoModalidad = $codigoModalidad
                                 WHERE codigo = $codigo");
     if ($cantidad > 0) {
-      echo json_encode(array('estado' => true));
+      echo json_encode(array('estado' => true, 'mensaje' => 'Admision actualizada'));
     } else {
-      echo json_encode(array('estado' => false));
+      echo json_encode(array('estado' => false, 'mensaje' => 'Error al actualizar admision'));
     }
   } catch (PDOException $e) {
-    echo json_encode(array('estado' => false,'mensaje'=>'Error al conectar con la base de datos'));
+    echo json_encode(array('estado' => false, 'mensaje' => 'Error al conectar con la base de datos'));
   }
 });
 
@@ -112,6 +113,31 @@ $app->delete('/api/universidades-objeto-disabled', function (Request $request) {
       echo json_encode(array('estado' => false));
     }
   } catch (PDOException $e) {
-    echo json_encode(array('estado' => false,'mensaje'=>'Error al conectar con la base de datos'));
+    echo json_encode(array('estado' => false, 'mensaje' => 'Error al conectar con la base de datos'));
+  }
+});
+
+$app->patch('/api/admision/{codigo}', function (Request $request) {
+  $codigo = $request->getAttribute('codigo');
+  $vigencia = ($request->getParam('vigencia')) ? 0 : 1;
+  try {
+    $carreras = $this->db->query("SELECT E.codigo from admision A INNER JOIN egresado E on E.codigoAdmision = A.codigo WHERE A.codigo = $codigo")->fetchAll();
+
+    if ($carreras) {
+      echo json_encode(array('estado' => false, 'mensaje' => 'Uy. Parece que tiene datos enlazados, no se puede eliminar'));
+      exit;
+    }
+
+
+    $cantidad = $this->db->exec("UPDATE admision set
+                                vigencia = $vigencia
+                                WHERE codigo = $codigo");
+    if ($cantidad > 0) {
+      echo json_encode(array('estado' => true, 'mensaje' => (!$vigencia) ? 'Admision eliminada, aun se puede recuperar' : 'Admision recuperada'));
+    } else {
+      echo json_encode(array('estado' => false, 'mensaje' => 'No se pudo actualizar la vigencia'));
+    }
+  } catch (PDOException $e) {
+    echo json_encode(array('estado' => false, 'mensaje' => 'Error al conectar con la base de datos'));
   }
 });
