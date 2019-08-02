4b509fe376d48486fe5d8a76a202ad4bce847dcb
JavCho23 <javier120699lili@gmail.com>
2019-08-01 21:57:38 -0500

    departamentos

--- servidor/src/routes/estadisticas.php
+++ servidor/src/routes/estadisticas.php
@@ -2,16 +2,19 @@
 
 use Psr\Http\Message\ServerRequestInterface as Request;
 
-$app->get('/api/estadisticas/distritos', function () {
+$app->get('/api/estadisticas/departamentos/{top}', function (Request $request) {
+    $top = $request->getAttribute('top');
+
     try {
-        $data = $this->db->query("SELECT D.nombre, COUNT(E.codigoPersona) as egresados
+        $data = $this->db->query("SELECT DE.nombre, COUNT( C.codigo ) as egresados
                                     FROM distrito D
                                     INNER JOIN centrolaboral CL on Cl.codigoDistrito = D.codigo
                                     INNER JOIN contrato C on C.codigoCentroLaboral = CL.codigo
-                                    INNER JOIN egresado E on E.codigo = C.codigoEgresado
-                                    GROUP by D.codigo
+                                    INNER JOIN provincia P on P.codigo = D.codigoProvincia
+                                    INNER JOIN departamento DE on P.codigoDepartamento = DE.codigo
+                                    GROUP by DE.codigo
                                     ORDER BY egresados DESC
-                                    LIMIT 10")->fetchAll();
+                                    LIMIT $top")->fetchAll();
         if ($data) {
             $result = array('estado' => true, 'data' => $data);
             echo json_encode($result);
@@ -23,7 +26,8 @@ $app->get('/api/estadisticas/distritos', function () {
     }
 });
 
-$app->get('/api/estadisticas/actividades', function () {
+$app->get('/api/estadisticas/actividades/{top}', function (Request $request) {
+    $top = $request->getAttribute('top');
     try {
         $data = $this->db->query("SELECT A.nombre, COUNT(C.codigo) as contratos 
                                 FROM actividadeconomica A
@@ -32,7 +36,7 @@ $app->get('/api/estadisticas/actividades', function () {
                                 INNER JOIN egresado E on E.codigo = C.codigoEgresado
                                 GROUP by A.codigo
                                 ORDER BY contratos DESC
-                                LIMIT 10")->fetchAll();
+                                LIMIT $top")->fetchAll();
         if ($data) {
             $result = array('estado' => true, 'data' => $data);
             echo json_encode($result);
@@ -44,14 +48,35 @@ $app->get('/api/estadisticas/actividades', function () {
     }
 });
 
-$app->get('/api/estadisticas/centros', function () {
+$app->get('/api/estadisticas/centros/{top}', function (Request $request) {
+    $top = $request->getAttribute('top');
     try {
         $data = $this->db->query("SELECT CE.razonSocial, COUNT(EP.codigo) as egresados
                                 FROM centroestudios CE
                                 INNER JOIN estudiospostgrado EP on EP.codigoCentroEstudios = CE.codigo
                                 GROUP by CE.codigo
                                 ORDER BY egresados DESC
-                                LIMIT 10")->fetchAll();
+                                LIMIT $top")->fetchAll();
+        if ($data) {
+            $result = array('estado' => true, 'data' => $data);
+            echo json_encode($result);
+        } else {
+            echo json_encode(array('estado' => false, 'mensaje' => 'No se han encontrado datos', 'data' => []));
+        }
+    } catch (PDOException $e) {
+        echo json_encode(array('estado' => false, 'mensaje' => 'Error al conectar con la base de datos'));
+    }
+});
+
+$app->get('/api/estadisticas/carreras/{carrera}', function (Request $request) {
+    $carrera = $request->getAttribute('carrera');
+    try {
+        $data = $this->db->query("SELECT CE.razonSocial, COUNT(EP.codigo) as egresados
+                                FROM centroestudios CE
+                                INNER JOIN estudiospostgrado EP on EP.codigoCentroEstudios = CE.codigo
+                                GROUP by CE.codigo
+                                ORDER BY egresados DESC
+                                LIMIT $top")->fetchAll();
         if ($data) {
             $result = array('estado' => true, 'data' => $data);
             echo json_encode($result);
