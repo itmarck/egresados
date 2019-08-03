2597eb01c98721d206933868ef92217b21fd33c9
JavCho23 <javier120699lili@gmail.com>
2019-08-03 02:55:03 -0500

    universidades

--- servidor/src/routes/estadisticas.php
+++ servidor/src/routes/estadisticas.php
@@ -37,9 +37,50 @@ $app->get('/api/estadisticas/total', function (Request $request) {
         
                                     FROM centroEstudios")->fetchAll();
         $data = [];
-        array_push($data, array('texto'=>'Usuarios registrados','valor' => $universidades[0]->U), array('texto'=>'Universidades registradas' ,'valor' => $egresados[0]->E), array('texto'=>'Centros de estudios registrados' ,'valor' => $centros[0]->C));
+        array_push($data, array('texto' => 'Usuarios registrados', 'valor' => $universidades[0]->U), array('texto' => 'Universidades registradas', 'valor' => $egresados[0]->E), array('texto' => 'Centros de estudios registrados', 'valor' => $centros[0]->C));
         if ($data) {
             $result = array('estado' => true, 'data' => $data);
+            echo json_encode($result);
+        } else {
+            echo json_encode(array('estado' => false, 'mensaje' => 'No se han encontrado datos', 'data' => []));
+        }
+    } catch (PDOException $e) {
+        echo json_encode(array('estado' => false, 'mensaje' => 'Error al conectar con la base de datos'));
+    }
+});
+
+$app->get('/api/estadisticas/universidades', function (Request $request) {
+
+    try {
+        $data = $this->db->query("SELECT U.siglas, COUNT( DISTINCT P.codigo ) as cantidad
+                                    FROM universidad U
+                                    INNER JOIN escuelaprofesional EP on EP.codigoUniversidad = U.codigo
+                                    INNER JOIN egresado E on E.codigoEscuela = EP.codigo
+                                    INNER JOIN persona P on P.codigo = E.codigoPersona 
+                                    GROUP by U.codigo
+                                    ORDER BY cantidad DESC")->fetchAll();
+        $total = $this->db->query("SELECT COUNT(codigo) as total from persona")->fetchAll();
+
+        if ($data) {
+            $porcentajes = [];
+            $categorias = [];
+            $sw = 1;
+            $aux = 0;
+            foreach ($data as $key => $cat) {
+                if ($sw <= 5) {
+                    $p = ($cat->cantidad / $total[0]->total) * 100;
+                    array_push($porcentajes, $p);
+                    array_push($categorias, $cat->siglas);
+                    $sw++;
+                } else {
+                    $aux = $aux + $cat->cantidad;
+                }
+            }
+            $aux = ($aux / $total[0]->total) * 100;
+            array_push($porcentajes, $aux);
+            array_push($categorias, 'Otros');
+
+            $result = array('estado' => true, 'data' => array('data' => $porcentajes, 'categories' => $categorias));
             echo json_encode($result);
         } else {
             echo json_encode(array('estado' => false, 'mensaje' => 'No se han encontrado datos', 'data' => []));
