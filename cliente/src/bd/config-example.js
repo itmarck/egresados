cfbeadd9b848958bcde8c601ce50d9bf64b7b9b3
JavCho23 <javier120699lili@gmail.com>
2019-08-04 15:50:45 -0500

    servidor probando

--- cliente/src/bd/config-example.js
+++ cliente/src/bd/config-example.js
@@ -1,6 +1,6 @@
 // URL de la api -- Cambiar de ser necesario
 
-const host = 'http://unprg.xyz/';
+const host = 'http://34.68.212.190/';
 
 export const url = host + 'egresados/servidor/src/public/api/';
 export const urlImage = host + 'egresados/servidor/src/public/images/';
